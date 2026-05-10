6c8347c697e6d0c7b29a6a127d0bb40681c6d8e6
qsxftt <qsxft.net@gmail.com>
2026-05-11 00:13:14 +0500

    Добавил простенькую коллизию

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from math import *
 from config import *
+from func import is_wall
 
 class Player:
     def __init__(self):
@@ -14,19 +15,28 @@ class Player:
         sin_a = sin(self.angle)
         cos_a = cos(self.angle)
 
+        dx = 0
+        dy = 0
+
         if keys[pygame.K_w]:
-            self.x += cos_a * self.speed
-            self.y += sin_a * self.speed
+            dx += cos_a * self.speed
+            dy += sin_a * self.speed
         if keys[pygame.K_s]:
-            self.x -= cos_a * self.speed
-            self.y -= sin_a * self.speed
+            dx -= cos_a * self.speed
+            dy -= sin_a * self.speed
         if keys[pygame.K_a]:
-            self.x += sin_a * self.speed
-            self.y -= cos_a * self.speed
+            dx += sin_a * self.speed
+            dy -= cos_a * self.speed
         if keys[pygame.K_d]:
-            self.x -= sin_a * self.speed
-            self.y += cos_a * self.speed
+            dx -= sin_a * self.speed
+            dy += cos_a * self.speed
         if keys[pygame.K_LEFT]:
-            self.angle -= 0.02 * self.speed 
+            self.angle -= 0.01 * self.speed 
         if keys[pygame.K_RIGHT]:
-            self.angle += 0.02 * self.speed
+            self.angle += 0.01 * self.speed
+        
+        if not is_wall(self.x + dx, self.y):
+                self.x += dx
+
+        if not is_wall(self.x, self.y + dy):
+            self.y += dy
